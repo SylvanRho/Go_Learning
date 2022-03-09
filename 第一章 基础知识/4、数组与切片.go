96283aac290b84e31782f10b17340e3514420a56
青 狄 <977434247@qq.com>
2022-03-09 15:45:19 +0800

    提交数组与切片

---
+++ 第一章 基础知识/4、数组与切片.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println()
+	ArrDemo()
+	SliceDemo()
+}
+
+//1、数组
+func ArrDemo() {
+	fmt.Println(`\n*******Array数组*******\n`)
+	var arr [3]int
+	arr[0] = 1
+	arr[1] = 2
+	arr[2] = 3
+
+	// 第一种方法
+	var arr2 [3]int = [3]int{1, 2, 3}
+	fmt.Printf("arr2 [0] value：%d\n", arr2[0])
+
+	// 第二种方法
+	arr3 := [3]int{1, 2, 3}
+	fmt.Printf("arr3 [0] value：%d\n", arr3[0])
+
+	//不定长数组
+	arr4 := [...]int{4, 6, 5}
+	fmt.Printf("arr4 [0] value：%d\n", arr4[0])
+
+	//%T打印类型
+	arr5 := [...]int{4, 6, 5, 7}
+	fmt.Printf("%d 的类型是: %T\n", arr4, arr4)
+	fmt.Printf("%d 的类型是: %T\n", arr5, arr5)
+
+	//别名类型
+	type arrLen3 [3]int
+	arr6 := arrLen3{1, 2, 3}
+	fmt.Printf("%d 的类型是: %T\n", arr6, arr6)
+
+	//偷懒的写法
+	arr7 := [4]int{2: 3}
+	fmt.Printf("arr7 的结构体是 %v", arr7)
+}
+
+//2、切片
+func SliceDemo() {
+	fmt.Println(`\n*******Slice切片*******\n`)
+	myarr01 := [...]int{1, 2, 3}
+	fmt.Printf("%d 的类型是: %T\n", myarr01[0:2], myarr01[0:2])
+
+	// 切片的构造，有四种方式
+	SliceDemo01()
+	SliceDemo02()
+	SliceDemo03()
+	SliceDemo04()
+}
+
+//1、对数组进行片段截取 主要有如下两种写法
+func SliceDemo01() {
+	myarr := [5]int{1, 2, 3, 4, 5}
+	fmt.Printf("myarr 的长度为：%d，容量为：%d\n", len(myarr), cap(myarr))
+
+	mysli1 := myarr[1:3] //3的含义为 索引为2（3减1）的元素
+	fmt.Printf("mysli1 的长度为：%d，容量为：%d\n", len(mysli1), cap(mysli1))
+	fmt.Println(mysli1)
+
+	mysli2 := myarr[1:3:4] //4的含义为容量范围从下标1到下标四
+	fmt.Printf("mysli2 的长度为：%d，容量为：%d\n", len(mysli2), cap(mysli2))
+	fmt.Println(mysli2)
+}
+
+//2、从头声明赋值（例子如下）
+func SliceDemo02() {
+	var strList []string
+	var numList []int
+	var numListEmpty = []int{}
+	fmt.Printf("strList 的长度为：%d，容量为：%d\n", len(strList), cap(strList))
+	fmt.Printf("numList 的长度为：%d，容量为：%d\n", len(numList), cap(numList))
+	fmt.Printf("numListEmpty 的长度为：%d，容量为：%d\n", len(numListEmpty), cap(numListEmpty))
+}
+
+//3、make函数构造
+func SliceDemo03() {
+	a := make([]int, 5)
+	b := make([]int, 2, 10)
+	fmt.Println(a, b)
+	fmt.Printf("a 的长度为 %d，容量为：%d\n", len(a), cap(a))
+	fmt.Printf("b 的长度为 %d，容量为：%d\n", len(b), cap(b))
+}
+
+//4、偷懒的写法
+func SliceDemo04() {
+	a := []int{4: 2}
+	fmt.Println(a)
+	fmt.Println(len(a), cap(a))
+}
