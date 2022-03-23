ef45bfc896ff2004d3aa771cb7322dc22ef14a98
青 狄 <977434247@qq.com>
2022-03-23 14:14:44 +0800

    提交空结构体的使用

---
+++ 2、第二章 面向对象/11、面向对象：Go 语言中的空结构体/11、面向对象：Go 语言中的空结构体.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func main() {
+	emptyStructUsedDemo()
+	chanUsedEmptyStructDemo()
+}
+
+//该对象没有属性，它就是一个空结构体。
+type lemp struct{}
+
+func (l lemp) On() {
+	fmt.Println("On")
+}
+
+func (l lemp) Off() {
+	fmt.Println("Off")
+}
+
+//空结构体的使用Demo
+func emptyStructUsedDemo() {
+	fmt.Printf("\n**********emptyStructUsedDemo**********\n")
+
+	l := lemp{}
+	l.On()
+	//空结构体是一个不占用空间的对象。
+	//output:0
+	fmt.Printf("lemp site：%d", unsafe.Sizeof(l))
+}
+
+//信道使用空结构体
+func chanUsedEmptyStructDemo() {
+	fmt.Printf("\n**********chanUsedEmptyStructDemo**********\n")
+
+	ch := make(chan struct{}, 1)
+	go func() {
+		<-ch
+		// do something
+	}()
+	ch <- struct{}{}
+	// ...
+}
