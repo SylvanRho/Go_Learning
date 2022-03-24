d1fba5915e1b990e91df3ecb9abd47d7c3aa0b7f
青 狄 <977434247@qq.com>
2022-03-24 16:08:56 +0800

    提交函数类型的属性

---
+++ 4、第四章 并发编程/2、学习Go函数：函数类型是什么？/2、学习Go函数：函数类型是什么？.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 转换成 Greeting 类型的函数对象
+	greet01 := Greeting(english)
+	// 或者
+	var greet02 Greeting = english
+
+	fmt.Println(greet01("xm01"))
+
+	fmt.Println(greet02("xm02"))
+
+	greet01.say("World")
+}
+
+//定义了一个传参是string  返回是string的函数类型属性
+type Greeting func(name string) string
+
+//跟Greeting函数类型相同 所以可以当成函数的实现
+func english(name string) string {
+	fmt.Printf("\n**********english**********\n")
+	return "Hello " + name
+}
+
+//greet 做为 Greeting 类型的对象，也拥有 Greeting 类型的所有方法，比如下面的 say 方法
+func (g Greeting) say(n string) {
+	fmt.Printf("\n**********say**********\n")
+	fmt.Println(g(n))
+}
